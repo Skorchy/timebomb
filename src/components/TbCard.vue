fa0e64f89bfe9d8ba7baeb05fd0cb432940b720c
Skorchy <rose.mickael.web@gmail.com>
2020-02-29 18:17:18 +0100

    Initial Commit

---
+++ src/components/TbCard.vue
@@ -0,0 +1,75 @@
+<template>
+  <!-- <div v-bind:class="{ active: isActive }"></div> -->
+  <!-- <div
+    class="card"
+    :class="{
+      'grey-card': cardType == 'grey' && turned,
+      'gold-card': cardType == 'bomb' && turned,
+      'red-card': cardType == 'bigben' && turned,
+      'brown-card': !turned
+    }"
+  /> -->
+  <FlipCard :flipped="!turned">
+    <template slot="front">
+      <div
+        @click="turned = false"
+        class="card"
+        :class="{
+          'grey-card': cardType == 'grey',
+          'gold-card': cardType == 'bomb',
+          'red-card': cardType == 'bigben'
+        }"
+      />
+    </template>
+    <template slot="back">
+      <div class="card brown-card" @click="turned = true" />
+    </template>
+  </FlipCard>
+</template>
+
+<script>
+import FlipCard from "@/components/FlipCard.vue";
+
+export default {
+  name: "TbCard",
+  components: {
+    FlipCard
+  },
+  props: {
+    cardType: {
+      validator: function(value) {
+        return ["grey", "bomb", "bigben"].includes(value);
+      },
+      required: true
+    }
+  },
+  data() {
+    return {
+      turned: false
+    };
+  }
+};
+</script>
+
+<style>
+.card {
+  margin: 5px;
+  width: 50px;
+  height: 90px;
+  border-radius: 7px;
+  border: 2px solid white;
+  display: inline-block;
+}
+.brown-card {
+  background-color: #803e29;
+}
+.grey-card {
+  background-color: #707070;
+}
+.gold-card {
+  background-color: #d4af0b;
+}
+.red-card {
+  background-color: #bf0202;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <TbCard cardType="grey" />
+    <TbCard cardType="bomb" />
+    <TbCard cardType="bigben" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TbCard from "@/components/TbCard.vue";
+
+export default {
+  name: "Home",
+  components: {
+    TbCard
+  }
+};
+</script>
